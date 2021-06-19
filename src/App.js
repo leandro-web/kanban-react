a7b0239a902f9dd6822c4bf8195be496825ba21d
leandro-web <leandro-web@hotmail.com>
2021-06-19 15:51:05 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
+import NavBar from "./components/NavBar/NavBar";
+import TaskList from "./components/TaskList/TaskList";
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc +1;
+  return idAcc;
+}
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
+  const updateTask = (id, title, state) => {
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if(task.id === id){
+          return { ...task, title, state };
+        }else{
+          return task;
+        }
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((existingTasks) =>{
+      return existingTasks.filter((task) => task.id !== id);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <div className="container">
+        <TaskList 
+          title={"Pendente"} 
+          onAddTask={addTask} 
+          taskState="Pendente"
+          tasks={tasks.filter((t) => t.state === "Pendente")} 
+          onTaskUpdate={updateTask} 
+          onDeleteTask={deleteTask}
+        />
+        <TaskList 
+          title={"Fazendo"} 
+          onAddTask={addTask} 
+          taskState="Fazendo"
+          tasks={tasks.filter((t) => t.state === "Fazendo")} 
+          onTaskUpdate={updateTask} 
+          onDeleteTask={deleteTask}
+        />
+        <TaskList 
+          title={"Completa"} 
+          onAddTask={addTask} 
+          taskState="Completa"
+          tasks={tasks.filter((t) => t.state === "Completa")} 
+          onTaskUpdate={updateTask} 
+          onDeleteTask={deleteTask}
+        />
+
+      </div>
     </div>
   );
 }
